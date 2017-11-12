41837fc364dca655696fb3cc512434ec12233e67
unknown <me@example.com>
2017-11-12 18:38:24 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,209 @@
+//	Web Service Update MySQL DB
+//	Usage:
+//			command prompt
+//			run 'node app.js'
+//			Console will print message
+//			Open URL in Browser to see message
+//  Version:  app.js.v3_good
+
+//Imports
+var express = require("express");
+var pg = require('pg');
+//var parse = require('utils-json-parse');
+var http = require('http');
+var bodyParser = require('body-parser');
+
+// Use Express
+var app = express()
+
+// Use body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Postgress DB Parameters - Heroku & Postgress
+	var con = pg.connectprocess.env.DATABASE_URL, function(err, client, done);
+// MySQL DB Parameters - Heroku & ClearDB
+//var con = mysql.createConnection({
+//	host: env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
+//	user: env('DB_USERNAME', 'b310795ff08cd3'),
+//	password: env('DB_PASSWORD', '24d90289'),
+//	database: env('DB_DATABASE', 'heroku_b344631383948cc')
+//  });
+// Local MySQL Parameters
+	//	var con = mysql.createConnection({
+//		host: "localhost",
+//		user: "admin",
+//		password: "admin",
+//		database: "students"
+//	  });
+
+// connect to DB
+    con.connect(function(err) {
+        if (err) {
+            console.error('Error:- ' + err.stack);
+            return;
+        }
+     
+        //console.log('Connected Id:- ' + con.threadId);
+    });
+
+// ------------------------------------------------
+// Express
+// ------------------------------------------------
+// GET request returns static text
+//app.get('/', (req, res) => res.send('Hello......node.js server'));
+app.get('/', function (req, res){
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.write('<h1>node.js DB app</h1><br /><br /> <strong>View All students:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/students">http:\/\/bmc-node-mysql.herokuapp.com\/students</a></br><strong>View Harry:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/harry">http:\/\/bmc-node-mysql.herokuapp.com\/harry</a></br><strong>Insert Student:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/insert">http:\/\/bmc-node-mysql.herokuapp.com\/insert</a></br><strong>Update Student Paid:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/update">http:\/\/bmc-node-mysql.herokuapp.com\/update</a></br> <strong>Update Student Free:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/update2">http:\/\/bmc-node-mysql.herokuapp.com\/update2</a></br><strong>Delete Student:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/delete">http:\/\/bmc-node-mysql.herokuapp.com\/delete</a></br></br></br></br></br></br><strong>Admin:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/admin">admin</a></br></br>');
+	res.end();
+});
+
+// GET request runs SELECT * query
+app.get('/students', function(request, response) {
+	con.query("SELECT * FROM students", function (err, result, fields) {
+		if (err) throw err;
+        console.log("Results:");
+ 		console.log(result);
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(JSON.stringify(result));
+		response.end();
+	  });
+});
+
+// GET request runs SELECT * query
+app.get('/harry', function(request, response) {
+	con.query("SELECT * FROM students WHERE lastname = 'Potter'", function (err, result, fields) {
+		if (err) throw err;
+        console.log("Results:");
+ 		console.log(result);
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(JSON.stringify(result));
+		response.end();
+	  });
+});
+
+// GET request runs INSERT query
+app.get('/insert', function(request, response) {
+	  var sql = "INSERT INTO students (firstname, lastname, email, institution, course, instructor, subscription_type, subscription_level) VALUES ('Mace', 'Windu', 'mwindu@ral.com', 'Rebel Alliance University', 'Rebellion History Insights', 'Yoda','','')";
+	  con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log("1 record inserted");
+		console.log(sql);
+	  });
+});
+
+// GET request runs UPDATE Paid query
+app.get('/update', function(request, response) {
+	  var value1 = '2';
+	  var value2 = '3';
+	  var sql = "UPDATE students SET subscription_type = 'Paid', subscription_level = 'Best' WHERE lastname = 'Granger'";
+	  con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log("1 record inserted");
+		console.log(sql);
+	  });
+	  
+});
+
+// GET request runs UPDATE Free query
+app.get('/update2', function(request, response) {
+    var value1 = '2';
+    var value2 = '3';
+    var sql = "UPDATE students SET subscription_type = 'Free', subscription_level = 'Good' WHERE lastname = 'Granger'";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+      console.log(sql);
+    });
+    
+});
+
+// GET request runs DELETE query
+app.get('/delete', function(request, response) {
+	   var sql = "DELETE FROM students WHERE lastname like '%Windu%'";
+	  con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log("1 record inserted");
+        console.log(sql);
+        response.write('Deleted student Mace Windu');
+	  });
+});
+
+// Handle POST with JSON
+app.post('/json', function(request, response) {
+    var jsonData = request.body;
+
+    // var myStringResult = JSON.stringify(request.body);
+    // var myResult = JSON.parse(myStringResult);
+    // firstname = myResult.firstname;
+    // lastname = myResult.lastname;
+    var jString = JSON.stringify(jsonData);
+    var jObj = JSON.parse(jString);
+    var tmp = 'New Student:  ' + jObj.firstname + ' ' + jObj.lastname + ' ' + jObj.email;
+    console.log('JSON = ' + tmp);
+
+    // Update Student
+    var sql = "UPDATE students SET subscription_type = '"+jObj.subscription_type+"', subscription_level = '"+jObj.subscription_level+"' WHERE lastname = '"+jObj.lastname+"'";
+    console.log(sql);
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+      
+    });
+    
+    // Write Response
+    response.set('Content-Type', 'application/json');
+    response.write("You sent some data " + JSON.stringify(jsonData));
+    response.end();
+});
+
+// ************************************************************************
+app.get('/admin', function (req, res){
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>ADMIN:  node.js DB app</h1><br /><br /> <strong>Create DB:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/createDB">http:\/\/bmc-node-mysql.herokuapp.com\/createDB</a></br><strong>Create Table:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/createTable">http:\/\/bmc-node-mysql.herokuapp.com\/createTable</a></br><strong>Load Data:  </strong><a href="http:\/\/bmc-node-mysql.herokuapp.com\/loadData">http:\/\/bmc-node-mysql.herokuapp.com\/loadData</a></br></br>');    
+    res.end();
+});
+
+// GET request runs Create Database query
+app.get('/createDB', function(request, response) {
+	con.query("CREATE DATABASE students", function (err, result) {
+		if (err) throw err;
+		console.log("Database created");
+  });
+});
+
+// GET request runs Create Table query
+app.get('/createTable', function(request, response) {
+	  var sql = "CREATE TABLE students (account_number INT AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255), email VARCHAR(255), institution VARCHAR(255), course VARCHAR(255), instructor VARCHAR(255), subscription_type VARCHAR(255), subscription_level VARCHAR(255))";
+	con.query(sql, function (err, result) {
+		if (err) throw err;
+		console.log("Table created");
+  });
+});
+
+// GET request runs Load Data INSERT query
+app.get('/loadData', function(request, response) {
+  var sql = "INSERT INTO students (firstname, lastname, email, institution, course, instructor, subscription_type, subscription_level) VALUES ?";
+  var values = [
+    ['Hermoine', 'Granger', 'hgranger@hogwarts.com', 'Hogwarts School of Witchcraft and Wizardry', 'Defense Against the Dark Arts', 'Severus Snape', 'Free', 'Good'],
+    ['Neval', 'Longbottom', 'nlongbottom@hogwarts.com', 'Hogwarts School of Witchcraft and Wizardry', 'Defense Against the Dark Arts', 'Severus Snape', 'Free', 'Good'],
+    ['Harry', 'Potter', 'hpotter@hogwarts.com', 'Hogwarts School of Witchcraft and Wizardry', 'Defense Against the Dark Arts', 'Severus Snape', 'Paid', 'Good'],
+    ['Ronald', 'Weasley', 'rweasley@hogwarts.com', 'Hogwarts School of Witchcraft and Wizardry', 'Defense Against the Dark Arts', 'Severus Snape', 'Free', 'Good'],
+    ['Poe', 'Dameron', 'pdameron@ral.com', 'Rebel Alliance University', 'X-Wing Combat Maneuvers', 'Obi Wan Kenobi', 'Free', 'Good'],
+    ['Luke', 'Skywalker', 'lskywalker@ral.com', 'Rebel Alliance University', 'X-Wing Combat Maneuvers', 'Obi Wan Kenobi','','']
+  ];
+  con.query(sql, [values], function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+});
+// ************************************************************************
+
+// Start Server
+var server = app.listen(process.env.PORT, function () {
+	//var host = server.address().address;
+	//var port = server.address().port;
+
+ //Print Message To Console
+ //console.log('Server running at http://%s:%s', host, port);
+});
